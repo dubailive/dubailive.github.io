c7d953a0e46b88c38c73ebf85ff36485d4929829
dubailive <dubailive1@gmail.com>
2016-08-31 02:06:07 +0300

    added buuf6

--- packages.sh
+++ packages.sh
@@ -4,3 +4,4 @@ dpkg-deb -bZgzip projects/buuf1 debs
 dpkg-deb -bZgzip projects/buuf3 debs
 dpkg-deb -bZgzip projects/buuf4 debs
 dpkg-deb -bZgzip projects/buuf5 debs
+dpkg-deb -bZgzip projects/buuf6 debs

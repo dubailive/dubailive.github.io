c07af1d799b01ba4a004cb352f21071b2f19292c
dubailive <dubailive1@gmail.com>
2016-09-03 18:16:13 +0300

    added essenza165

--- packages.sh
+++ packages.sh
@@ -10,3 +10,4 @@ dpkg-deb -bZgzip projects/essenza162 debs
 dpkg-deb -bZgzip projects/buuf7 debs
 dpkg-deb -bZgzip projects/essenza163 debs
 dpkg-deb -bZgzip projects/essenza164 debs
+dpkg-deb -bZgzip projects/essenza165 debs
